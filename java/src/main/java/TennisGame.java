3f84db88569089eed08192a01dbfb82c90a1ce94
Richards.Diejomaoh <rdiejomaoh@coronationmb.com>
2022-09-21 07:38:15 +0100

    Added an Enum

--- java/src/main/java/TennisGame.java
+++ java/src/main/java/TennisGame.java
@@ -2,4 +2,29 @@
 public interface TennisGame {
     void wonPoint(String playerName);
     String getScore();
+
+    static String getScoreOption1(int m_score1, int m_score2) {
+
+        String score = "";
+        //int tempScore = 0;
+
+        if (m_score1 == m_score2) {
+            switch (m_score1) {
+                case 0:
+                    score = ScoresEnum.LOVE_ALL.getScoreValue();
+                    break;
+                case 1:
+                    score = ScoresEnum.FIFTEEN_ALL.getScoreValue();
+                    break;
+                case 2:
+                    score = ScoresEnum.THIRTY_ALL.getScoreValue();
+                    break;
+                default:
+                    score = ScoresEnum.DEUCE.getScoreValue();
+                    break;
+            }
+
+        }
+        return score;
+    }
 }
